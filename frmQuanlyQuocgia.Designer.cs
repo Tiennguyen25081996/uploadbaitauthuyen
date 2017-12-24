83100e2c4e5440bd8327e73f344f4629ddf778b2
Tiennguyen25081996 <tiennguyen25081996@gmail.com>
2017-12-24 18:59:01 +0700

    Add files via upload

---
+++ frmQuanlyQuocgia.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace boat.GUI
+{
+    partial class frmQuanlyQuocgia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtmaquocgia = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txttenquocgia = new System.Windows.Forms.TextBox();
+            this.btnthem = new DevExpress.XtraEditors.SimpleButton();
+            this.btnsua = new DevExpress.XtraEditors.SimpleButton();
+            this.btnxoa = new DevExpress.XtraEditors.SimpleButton();
+            this.btnluu = new DevExpress.XtraEditors.SimpleButton();
+            this.btnhuy = new DevExpress.XtraEditors.SimpleButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Quốc Gia";
+            // 
+            // txtmaquocgia
+            // 
+            this.txtmaquocgia.Location = new System.Drawing.Point(147, 78);
+            this.txtmaquocgia.Name = "txtmaquocgia";
+            this.txtmaquocgia.Size = new System.Drawing.Size(100, 20);
+            this.txtmaquocgia.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(287, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tên Quốc Gia";
+            // 
+            // txttenquocgia
+            // 
+            this.txttenquocgia.Location = new System.Drawing.Point(379, 78);
+            this.txttenquocgia.Name = "txttenquocgia";
+            this.txttenquocgia.Size = new System.Drawing.Size(100, 20);
+            this.txttenquocgia.TabIndex = 2;
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(38, 122);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 23);
+            this.btnthem.TabIndex = 3;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(148, 122);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 23);
+            this.btnsua.TabIndex = 3;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(251, 122);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 23);
+            this.btnxoa.TabIndex = 3;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnluu
+            // 
+            this.btnluu.Location = new System.Drawing.Point(350, 122);
+            this.btnluu.Name = "btnluu";
+            this.btnluu.Size = new System.Drawing.Size(75, 23);
+            this.btnluu.TabIndex = 3;
+            this.btnluu.Text = "Lưu";
+            this.btnluu.Click += new System.EventHandler(this.btnluu_Click);
+            // 
+            // btnhuy
+            // 
+            this.btnhuy.Location = new System.Drawing.Point(450, 122);
+            this.btnhuy.Name = "btnhuy";
+            this.btnhuy.Size = new System.Drawing.Size(75, 23);
+            this.btnhuy.TabIndex = 3;
+            this.btnhuy.Text = "Hủy";
+            this.btnhuy.Click += new System.EventHandler(this.btnhuy_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 179);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(564, 283);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(197, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Quản Lý Quốc Gia";
+            // 
+            // frmQuanlyQuocgia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 462);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnhuy);
+            this.Controls.Add(this.btnluu);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.txttenquocgia);
+            this.Controls.Add(this.txtmaquocgia);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "frmQuanlyQuocgia";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmQuanlyQuocgia";
+            this.Load += new System.EventHandler(this.frmQuanlyQuocgia_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtmaquocgia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txttenquocgia;
+        private DevExpress.XtraEditors.SimpleButton btnthem;
+        private DevExpress.XtraEditors.SimpleButton btnsua;
+        private DevExpress.XtraEditors.SimpleButton btnxoa;
+        private DevExpress.XtraEditors.SimpleButton btnluu;
+        private DevExpress.XtraEditors.SimpleButton btnhuy;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ frmQuanlyQuocgia.cs
@@ -0,0 +1,92 @@
+﻿using boat.BUS;
+using boat.Model.DTO;
+using boat.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace boat.GUI
+{
+    public partial class frmQuanlyQuocgia : Form
+    {
+        QuocgiaBUS quocgiabus = new QuocgiaBUS();
+        public frmQuanlyQuocgia()
+        {
+            InitializeComponent();
+        }
+        int dem = 0;
+        void clear()
+        {
+            txtmaquocgia.Text = "";
+            txttenquocgia.Text = "";
+        }
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            clear();
+            dem = 1;
+        }
+        private void frmQuanlyQuocgia_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = quocgiabus.layDanhSachquocgia().Tables[0];
+            dataGridView1.Columns["ma"].HeaderText = "Mã Quốc Gia";
+            dataGridView1.Columns["tenquocgia"].HeaderText = "Tên Quốc Gia";
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtmaquocgia.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txttenquocgia.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+        }
+
+        private void btnluu_Click(object sender, EventArgs e)
+        {
+            if(dem == 1)
+            {
+                //them
+                quocgiabus.themquocgia(txtmaquocgia.Text,txttenquocgia.Text);
+                MessageBox.Show("Thêm Thành Công");
+                frmQuanlyQuocgia_Load(sender, e);
+            }
+            if(dem == 2)
+            {
+                //update
+                txtmaquocgia.Enabled = true;
+                quocgiabus.suaquocgia(txtmaquocgia.Text, txttenquocgia.Text);
+                MessageBox.Show("Sửa Thành Công");
+                frmQuanlyQuocgia_Load(sender, e);
+            }
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("bạn có chắc muốn xóa không ?", "thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+
+                quocgiabus.xoaQUOCGIA(txtmaquocgia.Text);
+                MessageBox.Show("Xóa Thành Công");
+                clear();
+                frmQuanlyQuocgia_Load(sender, e);
+            }
+           
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            txtmaquocgia.Enabled = true;
+            dem = 2;
+        }
+
+        private void btnhuy_Click(object sender, EventArgs e)
+        {
+            clear();
+
+        }
+    }
+}
